f23d8b6cf1ee697bf945552c19a69fb4635277ac
Michael Schwarz <michi.schwarz@gmail.com>
2025-06-07 12:00:36 +0200

    Get rid of Popen wrapper

--- src/git_helpers/commands/snap.py
+++ src/git_helpers/commands/snap.py
@@ -1,71 +1,28 @@
 import argparse
-import subprocess
 import sys
 from argparse import Namespace
 from subprocess import PIPE
-from typing import IO
-from typing import Any
+from subprocess import run
 
 from git_helpers.util import get_commit_message
 from git_helpers.util import pass_parsed_args
 
 
-class CommandResult:
-    def __init__(self, code: int, out: bytes, err: bytes):
-        self.code = code
-        self.out = out
-        self.err = err
-
-
-def command(
-    *args: str,
-    use_stdout: bool = False,
-    use_stderr: bool = False,
-    stdout_on_stderr: bool = False,
-    allow_error: bool = False,
-    cwd: str | None = None,
-) -> CommandResult:
-    if use_stdout:
-        assert not stdout_on_stderr
-
-        stdout: int | IO[Any] = PIPE
-    elif stdout_on_stderr:
-        stdout = sys.stderr.buffer
-    else:
-        stdout = sys.stdout
-
-    if use_stderr:
-        stderr: int | IO[Any] = PIPE
-    else:
-        stderr = sys.stderr
-
-    process = subprocess.Popen(args, stdout=stdout, stderr=stderr, cwd=cwd)
-    out, err = process.communicate()
-    res = CommandResult(process.returncode, out, out)
-
-    if not allow_error:
-        assert not res.code
-
-    return res
-
-
 def get_branch_name(ref: str) -> str | None:
-    res = command(
-        "git", "symbolic-ref", "-q", "--short", ref, allow_error=True, use_stdout=True
-    )
+    result = run(["git", "symbolic-ref", "-q", "--short", ref], stdout=PIPE)
 
-    if res.code:
+    if result.returncode:
         return None
     else:
-        return res.out.decode().strip()
+        return result.stdout.decode().strip()
 
 
 def has_staged_changes() -> bool:
-    return command("git", "diff", "--cached", "--quiet", allow_error=True).code > 0
+    return run(["git", "diff", "--cached", "--quiet"]).returncode > 0
 
 
 def stage_all() -> None:
-    command("git", "add", "--all", ":/")
+    run(["git", "add", "--all", ":/"])
 
 
 def commit(message: str) -> None:
@@ -74,7 +31,7 @@ def commit(message: str) -> None:
     if message == get_commit_message("HEAD"):
         args.append("--amend")
 
-    command("git", "commit", *args, stdout_on_stderr=True)
+    run(["git", "commit", *args], stdout=sys.stderr.buffer)
 
 
 def parse_args() -> Namespace:
@@ -99,4 +56,4 @@ def entry_point(subject_words: list[str]) -> None:
     if has_staged_changes():
         commit(message)
 
-    command("git", "status")
+    run(["git", "status"])
